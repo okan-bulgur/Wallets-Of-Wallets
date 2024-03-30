276008da9b219705cf6877f6a5c32c4eae282430
Yavuz Sayar <140386617+yavuz-sayar@users.noreply.github.com>
2024-03-30 22:33:47 +0300

    main page changed

--- wallets_of_wallets/lib/main_page.dart
+++ wallets_of_wallets/lib/main_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api, use_super_parameters
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api, use_super_parameters, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -31,32 +31,48 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wallets of Wallets'),
+        title: const Text(
+          'Wallets of Wallets',
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 54, 106, 57)
+          ),
+        ),        
         centerTitle: true,
         automaticallyImplyLeading: false,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              // Add functionality for the avatar button
+            },
+            child: CircleAvatar(
+              radius: 40.0, // Adjust radius as needed
+              backgroundColor: Colors.white, // Change background color if needed
+              child: Icon(
+                Icons.account_circle,
+                size:40,
+                color: Color.fromARGB(255, 54, 106, 57), // Change icon color if needed
+              ),
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu),
-            label: 'Main Menu',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_balance_wallet),
-            label: 'Wallets',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromARGB(255, 77, 125, 79), // Change the selected item color
-        onTap: _onItemTapped,
-      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add functionality for the plus button
+        },
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        child: Icon(
+          Icons.add,
+          size: 50, // Adjust size of the icon
+          color: Color.fromARGB(255, 54, 106, 57),
+        ), // Change background color if needed
+      ),    
     );
   }
 }
