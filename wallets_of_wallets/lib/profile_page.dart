2afeb3c3adf57322ad2f96fff76c85c092a54170
Yavuz Sayar <140386617+yavuz-sayar@users.noreply.github.com>
2024-05-14 22:25:56 +0300

    profile page input format modified
    + Profile page Amount kısmı sadece floating point input alacak şekilde değiştirildi.
    
    + Profile photo input şekli kontrol edildi sadece fotoğraf alıyor gibi gözüküyor.

--- wallets_of_wallets/lib/profile_page.dart
+++ wallets_of_wallets/lib/profile_page.dart
@@ -9,6 +9,7 @@ import 'package:firstly/data_base_manager.dart';
 import 'package:firstly/main_page.dart';
 import 'package:firstly/profile_settings_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'login_page.dart'; // Import your login page widget
 
 class ProfilePage extends StatefulWidget {
@@ -16,15 +17,14 @@ class ProfilePage extends StatefulWidget {
   _ProfilePageState createState() => _ProfilePageState();
 }
 
-
 class _ProfilePageState extends State<ProfilePage> {
   final Color customColor = Color(0xFF0A5440);
 
   String selectedUser = ''; // Default user
   double balance = 0.00;
 
-    // Get the current user's email
-    
+  // Get the current user's email
+
   String? userEmail = FirebaseAuth.instance.currentUser!.email;
 
   TextEditingController card = TextEditingController();
@@ -40,7 +40,10 @@ class _ProfilePageState extends State<ProfilePage> {
   Future<void> _fetchUserData() async {
     try {
       // Retrieve user data from Firestore
-      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection('users').doc(userEmail).get();
+      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userEmail)
+          .get();
 
       // Extract user information
       if (userSnapshot.exists) {
@@ -59,7 +62,10 @@ class _ProfilePageState extends State<ProfilePage> {
   Future<void> _fetchUserBalance() async {
     try {
       // Retrieve user data from Firestore
-      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection('users').doc(userEmail).get();
+      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userEmail)
+          .get();
 
       // Extract user information
       if (userSnapshot.exists) {
@@ -72,8 +78,6 @@ class _ProfilePageState extends State<ProfilePage> {
       print("Error fetching user data: $e");
     }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -113,68 +117,75 @@ class _ProfilePageState extends State<ProfilePage> {
           CircleAvatar(
             radius: 65,
             //if userPhoto is empty String, show default image
-            backgroundImage: userPhoto!.isEmpty ? AssetImage('assets/pp_1.png') as ImageProvider<Object> : NetworkImage(userPhoto!),
+            backgroundImage: userPhoto!.isEmpty
+                ? AssetImage('assets/pp_1.png') as ImageProvider<Object>
+                : NetworkImage(userPhoto!),
           ),
           SizedBox(height: 20),
           Text(
-              '₺$balance',
-              style: TextStyle(
+            '₺$balance',
+            style: TextStyle(
                 fontSize: 40.0,
                 fontWeight: FontWeight.bold,
-                color: customColor
-              ),
+                color: customColor),
           ),
           SizedBox(height: 20),
           Text(
             '$selectedUser',
             style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: customColor
-            ),
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: customColor),
           ),
           SizedBox(height: 20),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:
-            DropdownButtonFormField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Color.fromARGB(100, 150, 150, 150),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0), // Adjust the radius for curved edges
-                    borderSide:
-                    BorderSide.none, // Set the border color to gray
-                  ),
-                ),
-                items: [
-                  for (UserCard card in CardManager.cards)
-                    DropdownMenuItem<String>(
-                      value: card.cardCardName,
-                      child: Text(card.cardCardName),
+              padding: const EdgeInsets.all(8.0),
+              child: DropdownButtonFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color.fromARGB(100, 150, 150, 150),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                          15.0), // Adjust the radius for curved edges
+                      borderSide:
+                          BorderSide.none, // Set the border color to gray
                     ),
-                ],
-                onChanged: (String? value) {
-                  // This is called when the user selects an item.
-                  setState(() {
-                    card.text = value!;
-                  },);
-                },
-                isExpanded: true)
-            ),
+                  ),
+                  items: [
+                    for (UserCard card in CardManager.cards)
+                      DropdownMenuItem<String>(
+                        value: card.cardCardName,
+                        child: Text(card.cardCardName),
+                      ),
+                  ],
+                  onChanged: (String? value) {
+                    // This is called when the user selects an item.
+                    setState(
+                      () {
+                        card.text = value!;
+                      },
+                    );
+                  },
+                  isExpanded: true)),
           SizedBox(height: 10),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               controller: amount,
+              inputFormatters: <TextInputFormatter>[
+                //only 2 digits after the decimal point
+                FilteringTextInputFormatter.allow(RegExp(
+                        r'^\d+\.?\d{0,2}') //only 2 digits after the decimal point
+                    ),
+              ],
               decoration: InputDecoration(
                 labelText: 'Amount',
                 filled: true,
                 fillColor: Color.fromARGB(100, 150, 150, 150),
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0), // Adjust the radius for curved edges
-                  borderSide:
-                  BorderSide.none, // Set the border color to gray
+                  borderRadius: BorderRadius.circular(
+                      15.0), // Adjust the radius for curved edges
+                  borderSide: BorderSide.none, // Set the border color to gray
                 ),
               ),
             ),
@@ -184,7 +195,8 @@ class _ProfilePageState extends State<ProfilePage> {
             child: SizedBox(
               width: 300.0, // Adjust the width as needed
               child: ElevatedButton(
-                onPressed: () async{ // Yavuz : async added because we are using await in the function
+                onPressed: () async {
+                  // Yavuz : async added because we are using await in the function
                   //check if card selected using textEditingController
                   if (card.text.isEmpty) {
                     ScaffoldMessenger.of(context).showSnackBar(
@@ -196,12 +208,14 @@ class _ProfilePageState extends State<ProfilePage> {
                     return;
                   }
                   double withdrawalAmount = double.parse(amount.text);
-                  await UsersTableManager.updateUserBalance(context, userEmail!, withdrawalAmount, false);
+                  await UsersTableManager.updateUserBalance(
+                      context, userEmail!, withdrawalAmount, false);
                   _fetchUserBalance();
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: customColor, // Set the button color
-                  minimumSize: Size(120.0, 40.0), // Set the minimum size of the button
+                  minimumSize:
+                      Size(120.0, 40.0), // Set the minimum size of the button
                 ),
                 child: Text(
                   'Withdraw',
@@ -219,7 +233,8 @@ class _ProfilePageState extends State<ProfilePage> {
             child: SizedBox(
               width: 300.0, // Adjust the width as needed
               child: ElevatedButton(
-                onPressed: () async { // Yavuz: async added because we are using await in the function
+                onPressed: () async {
+                  // Yavuz: async added because we are using await in the function
                   //check if card selected using textEditingController
                   if (card.text.isEmpty) {
                     ScaffoldMessenger.of(context).showSnackBar(
@@ -231,12 +246,14 @@ class _ProfilePageState extends State<ProfilePage> {
                     return;
                   }
                   double depositAmount = double.parse(amount.text);
-                  await UsersTableManager.updateUserBalance(context, userEmail!, depositAmount, true);
+                  await UsersTableManager.updateUserBalance(
+                      context, userEmail!, depositAmount, true);
                   _fetchUserBalance();
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: customColor, // Set the button color
-                  minimumSize: Size(120.0, 40.0), // Set the minimum size of the button
+                  minimumSize:
+                      Size(120.0, 40.0), // Set the minimum size of the button
                 ),
                 child: Text(
                   'Deposit',
@@ -266,34 +283,41 @@ class _ProfilePageState extends State<ProfilePage> {
             label: 'Settings',
           ),
         ],
-        
+
         selectedItemColor: customColor, // Set color for selected icon
-        selectedFontSize: double.parse('14.5'), // Set font size for selected label
-        
+        selectedFontSize:
+            double.parse('14.5'), // Set font size for selected label
+
         showUnselectedLabels: true,
         unselectedItemColor: customColor, // Set color for unselected icon
-        unselectedFontSize: double.parse('14.5'), // Set font size for unselected label
-        unselectedLabelStyle: TextStyle(color: customColor), // Set color for unselected label
-        
+        unselectedFontSize:
+            double.parse('14.5'), // Set font size for unselected label
+        unselectedLabelStyle:
+            TextStyle(color: customColor), // Set color for unselected label
+
         onTap: (index) {
           switch (index) {
             case 0:
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => MainPage()), // Navigate to MainPage
+                MaterialPageRoute(
+                    builder: (context) => MainPage()), // Navigate to MainPage
               );
               break;
             case 1:
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => CardsList()), // Navigate to MainPage
+                MaterialPageRoute(
+                    builder: (context) => CardsList()), // Navigate to MainPage
               );
               break;
               break;
             case 2:
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => ProfileSettings()), // Navigate to MainPage
+                MaterialPageRoute(
+                    builder: (context) =>
+                        ProfileSettings()), // Navigate to MainPage
               );
               break;
           }
